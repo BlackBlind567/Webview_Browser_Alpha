af98ee5a52f689fa716a0fb1a5867c61a46b1b03
BlackBlind567 <shantanujaiswal20@gmail.com>
2018-03-12 19:31:37 +0530

    menu list updation BBS

--- app/src/main/java/rscipher001/webbrowser/MainActivity.java
+++ app/src/main/java/rscipher001/webbrowser/MainActivity.java
@@ -10,41 +10,50 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    WebView myWebView;
-    EditText eturl;
-    Button submit;
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_menu, menu);
-        return true;
-    }
+//    WebView myWebView;
+//    EditText eturl;
+//    Button search;
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        MenuInflater inflater = getMenuInflater();
+//        inflater.inflate(R.menu.main_menu, menu);
+//        return true;
+//    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        myWebView = findViewById(R.id.webView);
-        eturl = findViewById(R.id.url);
-        submit = findViewById(R.id.submit);
-
-        myWebView.setWebViewClient(new WebViewClient());
-        WebSettings myWebSettings = myWebView.getSettings();
-        myWebSettings.setJavaScriptEnabled(true);
-//        myWebView.loadUrl("https://www.whatismybrowser.com/detect/are-third-party-cookies-enabled");
-        myWebView.loadUrl("https://www.google.co.in/");
-
-        submit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                myWebView.loadUrl(eturl.getText().toString());
-            }
-        });
+//        myWebView = findViewById(R.id.webView);
+//        eturl = findViewById(R.id.url);
+//        search =  findViewById(R.id.search);
+//
+//        myWebView.setWebViewClient(new WebViewClient());
+//        WebSettings myWebSettings = myWebView.getSettings();
+//        myWebSettings.setJavaScriptEnabled(true);
+//        myWebView.loadUrl("https://www.google.co.in/");
+//
+//        search.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                String url = eturl.getText().toString();
+//                String http = "http://";
+//
+//                if (url.substring(0, 6).equals(http)) {
+//                    myWebView.loadUrl(url);
+//                } else {
+//                    myWebView.loadUrl("http://" + url);
+//                }
+//            }
+//        });
 
 
     }
